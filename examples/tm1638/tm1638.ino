6897fc49f9dcd17b78a09c091d4aacb6f5e52a04
javacasm <javacasm@gmail.com>
2015-10-22 00:25:14 +0200

    tm1638

--- examples/tm1638/tm1638.ino
+++ examples/tm1638/tm1638.ino
@@ -18,8 +18,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <TM1638.h>
 
-// define a module on data pin 8, clock pin 9 and strobe pin 7
-TM1638 module(8, 9, 7);
+#define dataPin 8
+#define CLKPin 9
+#define StrobePin 10
+TM1638 module(dataPin, CLKPin, StrobePin);
 
 void setup() {
   // display a hexadecimal number and set the left 4 dots
